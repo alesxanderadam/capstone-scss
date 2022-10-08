fc66e0efe4cfe76a803a1443a2c84c631b946764
ltqhuy0112 <ltqhuy.th0112@gmail.com>
2022-10-08 13:22:54 +0700

    Lệnh push đầu tiên

---
+++ javascript/settingSlick.js
@@ -0,0 +1,25 @@
+$('.animation').slick({
+    // autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+});
